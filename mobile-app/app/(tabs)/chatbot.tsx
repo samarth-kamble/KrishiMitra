a95cf43e72ee33c903b054808158b039d198acb1
shivajirp <shivajirp123@gmail.com>
2025-03-28 03:19:08 +0530

    added chatbot

---
+++ mobile-app/app/(tabs)/chatbot.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const chatbot = () => {
+  return (
+    <View>
+      <Text>chatbot</Text>
+    </View>
+  )
+}
+
+export default chatbot
+
+const styles = StyleSheet.create({})
