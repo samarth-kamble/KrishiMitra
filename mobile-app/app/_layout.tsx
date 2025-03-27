d5b88a2ab516edfc1047dd106b7f76944d910717
Samarth Kamble <kamblesamarth04@gmail.com>
2025-03-28 04:14:10 +0530

    add StatusBar import to layout and clean up package.json dependencies

--- mobile-app/app/_layout.tsx
+++ mobile-app/app/_layout.tsx
@@ -1,6 +1,7 @@
 import { Stack } from "expo-router";
 import "./globals.css";
 import { useFonts } from "expo-font";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
   const [loaded] = useFonts({
