da2ec3d8f2a54f86d1e869449c1b4f6970b0785c
shivajirp <shivajirp123@gmail.com>
2025-03-28 03:08:45 +0530

    merged

--- mobile-app/app/index.tsx
+++ mobile-app/app/index.tsx
@@ -3,7 +3,12 @@ import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Page = () => {
-  return <Redirect href={"/(auth)/welcome"} />;
+  // return <Redirect href={"/(auth)/welcome"} />;
+  return (
+    <SafeAreaView>
+      <Link href="/home">Go to dashboard</Link>
+    </SafeAreaView>
+  )
 };
 
 export default Page;
